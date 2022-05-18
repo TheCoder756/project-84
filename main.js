b9820d75c76061ea6fec0addd6ba1ce0dad52955
TheCoder756 <92489997+TheCoder756@users.noreply.github.com>
2022-05-18 18:51:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+
+	
